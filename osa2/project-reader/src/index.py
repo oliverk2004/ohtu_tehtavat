37ca8430cf7c4c7ebf46725dc97b99c0a4a0e4f7
Oliver Kandén <oliver.kanden04@gmail.com>
2025-11-07 14:25:40 +0200

    Pushataan pyproject.toml urlia varten

--- osa2/project-reader/src/index.py
+++ osa2/project-reader/src/index.py
@@ -2,7 +2,7 @@ from project_reader import ProjectReader
 
 
 def main():
-    url = "https://raw.githubusercontent.com/ohjelmistotuotanto-jyu/tehtavat/main/osa2/project-reader/pyproject.toml"
+    url = "https://raw.githubusercontent.com/oliverk2004/ohtu_tehtavat/main/osa2/project-reader/pyproject.toml"
     reader = ProjectReader(url)
     print(reader.get_project())
 
